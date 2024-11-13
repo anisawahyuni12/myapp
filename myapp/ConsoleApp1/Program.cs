21783e4c07ac5ad52f311aabb3fe05482ea4671d
anisawahyuni <anisawahyuni121@gmail.com>
2024-11-13 09:38:58 +0700

    l

---
+++ myapp/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("siapa namamu?");
+var namamu = Console.ReadLine();
+
+Console.WriteLine("DImana rumahmu");
+var rumah = Console.ReadLine ();
+
+Console.WriteLine("apa impianmu");
+var impian = Console.ReadLine ();
+
+Console.WriteLine("nama saya adalah" + namamu);
+Console.WriteLine("rumah saya" + rumah);
+Console.WriteLine("impian saya" +  impian);
